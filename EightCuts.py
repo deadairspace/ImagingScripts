1e326ada817af8869f5141c2847bcebc3795ee25
Steven Hahn <hahnse@ornl.gov>
2016-03-02 10:51:33 -0500

    Cleanup EightCuts script.

--- EightCuts.py
+++ EightCuts.py
@@ -3,6 +3,8 @@ from paraview.simple import *
 #### disable automatic camera reset on 'Show'
 paraview.simple._DisableFirstRenderCameraReset()
 
+LoadPlugin('/Users/svh/Documents/MantidProject/ParaView-build-release/bin/paraview.app/Contents/MacOS/../../../../lib/libAcceleratedAlgorithms.dylib', remote=False,ns=globals())
+
 # create a new 'XML Image Data Reader'
 test1p0vti = XMLImageDataReader(FileName=['/Users/svh/Documents/IterateImageData/release/test1p0.vti'])
 test1p0vti.PointArrayStatus = ['ImageScalars']
@@ -24,36 +26,12 @@ test1p0vtiDisplay.Slice = 226
 # reset view to fit data
 renderView1.ResetCamera()
 
-# reset view to fit data
-renderView1.ResetCamera()
-
-# create a new 'Flying Edges 2D'
-flyingEdges2D1 = FlyingEdges2D(Input=test1p0vti)
-flyingEdges2D1.ContourBy = ['POINTS', 'ImageScalars']
-flyingEdges2D1.Isosurfaces = [0.12182831950485706]
-
 # set active source
 SetActiveSource(test1p0vti)
-
-# destroy flyingEdges2D1
-Delete(flyingEdges2D1)
-del flyingEdges2D1
 
 # create a new 'Flying Edges Plane Cutter'
 flyingEdgesPlaneCutter1 = FlyingEdgesPlaneCutter(Input=test1p0vti)
 flyingEdgesPlaneCutter1.SliceType = 'Plane'
-
-# init the 'Plane' selected for 'SliceType'
-flyingEdgesPlaneCutter1.SliceType.Origin = [240.284, 240.284, 904.0]
-
-# reset view to fit data
-renderView1.ResetCamera()
-
-# reset view to fit data
-renderView1.ResetCamera()
-
-# reset view to fit data
-renderView1.ResetCamera()
 
 # reset view to fit data
 renderView1.ResetCamera()
@@ -104,9 +82,6 @@ SetActiveSource(test1p0vti)
 flyingEdgesPlaneCutter2 = FlyingEdgesPlaneCutter(Input=test1p0vti)
 flyingEdgesPlaneCutter2.SliceType = 'Plane'
 
-# init the 'Plane' selected for 'SliceType'
-flyingEdgesPlaneCutter2.SliceType.Origin = [240.284, 240.284, 904.0]
-
 # Properties modified on flyingEdgesPlaneCutter2.SliceType
 flyingEdgesPlaneCutter2.SliceType.Origin = [0.0, 0.0, 400.0]
 flyingEdgesPlaneCutter2.SliceType.Normal = [0.0, 0.0, 1.0]
@@ -137,18 +112,9 @@ flyingEdgesPlaneCutter2Display.SetScalarBarVisibility(renderView1, True)
 # set active source
 SetActiveSource(test1p0vti)
 
-# set active source
-SetActiveSource(flyingEdgesPlaneCutter2)
-
-# set active source
-SetActiveSource(test1p0vti)
-
 # create a new 'Flying Edges Plane Cutter'
 flyingEdgesPlaneCutter3 = FlyingEdgesPlaneCutter(Input=test1p0vti)
 flyingEdgesPlaneCutter3.SliceType = 'Plane'
-
-# init the 'Plane' selected for 'SliceType'
-flyingEdgesPlaneCutter3.SliceType.Origin = [240.284, 240.284, 904.0]
 
 # Properties modified on flyingEdgesPlaneCutter3.SliceType
 flyingEdgesPlaneCutter3.SliceType.Origin = [0.0, 0.0, 600.0]
@@ -184,9 +150,6 @@ SetActiveSource(test1p0vti)
 flyingEdgesPlaneCutter4 = FlyingEdgesPlaneCutter(Input=test1p0vti)
 flyingEdgesPlaneCutter4.SliceType = 'Plane'
 
-# init the 'Plane' selected for 'SliceType'
-flyingEdgesPlaneCutter4.SliceType.Origin = [240.284, 240.284, 904.0]
-
 # Properties modified on flyingEdgesPlaneCutter4.SliceType
 flyingEdgesPlaneCutter4.SliceType.Origin = [0.0, 0.0, 800.0]
 flyingEdgesPlaneCutter4.SliceType.Normal = [0.0, 0.0, 1.0]
@@ -221,9 +184,6 @@ SetActiveSource(test1p0vti)
 flyingEdgesPlaneCutter5 = FlyingEdgesPlaneCutter(Input=test1p0vti)
 flyingEdgesPlaneCutter5.SliceType = 'Plane'
 
-# init the 'Plane' selected for 'SliceType'
-flyingEdgesPlaneCutter5.SliceType.Origin = [240.284, 240.284, 904.0]
-
 # Properties modified on flyingEdgesPlaneCutter5.SliceType
 flyingEdgesPlaneCutter5.SliceType.Origin = [0.0, 0.0, 1000.0]
 flyingEdgesPlaneCutter5.SliceType.Normal = [0.0, 0.0, 1.0]
@@ -239,15 +199,24 @@ flyingEdgesPlaneCutter5Display.ScaleTransferFunction = 'PiecewiseFunction'
 flyingEdgesPlaneCutter5Display.OpacityArray = ['POINTS', 'ImageScalars']
 flyingEdgesPlaneCutter5Display.OpacityTransferFunction = 'PiecewiseFunction'
 
+# change representation type
+flyingEdgesPlaneCutter5Display.SetRepresentationType('Surface')
+
+# set scalar coloring
+ColorBy(flyingEdgesPlaneCutter5Display, ('POINTS', 'ImageScalars'))
+
+# rescale color and/or opacity maps used to include current data range
+flyingEdgesPlaneCutter5Display.RescaleTransferFunctionToDataRange(True)
+
+# show color bar/color legend
+flyingEdgesPlaneCutter5Display.SetScalarBarVisibility(renderView1, True)
+
 # set active source
 SetActiveSource(test1p0vti)
 
 # create a new 'Flying Edges Plane Cutter'
 flyingEdgesPlaneCutter6 = FlyingEdgesPlaneCutter(Input=test1p0vti)
 flyingEdgesPlaneCutter6.SliceType = 'Plane'
-
-# init the 'Plane' selected for 'SliceType'
-flyingEdgesPlaneCutter6.SliceType.Origin = [240.284, 240.284, 904.0]
 
 # Properties modified on flyingEdgesPlaneCutter6.SliceType
 flyingEdgesPlaneCutter6.SliceType.Origin = [240.0, 0.0, 1200.0]
@@ -268,6 +237,7 @@ flyingEdgesPlaneCutter6Display.OpacityTransferFunction = 'PiecewiseFunction'
 flyingEdgesPlaneCutter6Display.SetRepresentationType('Surface')
 
 # Properties modified on flyingEdgesPlaneCutter6.SliceType
+flyingEdgesPlaneCutter6.SliceType.Origin = [0.0, 0.0, 1200.0]
 flyingEdgesPlaneCutter6.SliceType.Normal = [0.0, 0.0, 1.0]
 
 # Properties modified on flyingEdgesPlaneCutter6Display
@@ -291,9 +261,6 @@ SetActiveSource(test1p0vti)
 # create a new 'Flying Edges Plane Cutter'
 flyingEdgesPlaneCutter7 = FlyingEdgesPlaneCutter(Input=test1p0vti)
 flyingEdgesPlaneCutter7.SliceType = 'Plane'
-
-# init the 'Plane' selected for 'SliceType'
-flyingEdgesPlaneCutter7.SliceType.Origin = [240.284, 240.284, 904.0]
 
 # Properties modified on flyingEdgesPlaneCutter7.SliceType
 flyingEdgesPlaneCutter7.SliceType.Origin = [0.0, 0.0, 1400.0]
@@ -331,20 +298,11 @@ flyingEdgesPlaneCutter7Display.RescaleTransferFunctionToDataRange(True)
 flyingEdgesPlaneCutter7Display.SetScalarBarVisibility(renderView1, True)
 
 # set active source
-SetActiveSource(flyingEdgesPlaneCutter6)
-
-# Properties modified on flyingEdgesPlaneCutter6.SliceType
-flyingEdgesPlaneCutter6.SliceType.Origin = [0.0, 0.0, 1200.0]
-
-# set active source
 SetActiveSource(test1p0vti)
 
 # create a new 'Flying Edges Plane Cutter'
 flyingEdgesPlaneCutter8 = FlyingEdgesPlaneCutter(Input=test1p0vti)
 flyingEdgesPlaneCutter8.SliceType = 'Plane'
-
-# init the 'Plane' selected for 'SliceType'
-flyingEdgesPlaneCutter8.SliceType.Origin = [240.284, 240.284, 904.0]
 
 # Properties modified on flyingEdgesPlaneCutter8.SliceType
 flyingEdgesPlaneCutter8.SliceType.Origin = [0.0, 0.0, 1600.0]
@@ -376,67 +334,6 @@ flyingEdgesPlaneCutter8Display.SetScalarBarVisibility(renderView1, True)
 # set active source
 SetActiveSource(test1p0vti)
 
-# create a new 'Flying Edges Plane Cutter'
-flyingEdgesPlaneCutter9 = FlyingEdgesPlaneCutter(Input=test1p0vti)
-flyingEdgesPlaneCutter9.SliceType = 'Plane'
-
-# init the 'Plane' selected for 'SliceType'
-flyingEdgesPlaneCutter9.SliceType.Origin = [240.284, 240.284, 904.0]
-
-# set active source
-SetActiveSource(flyingEdgesPlaneCutter1)
-
-# set active source
-SetActiveSource(flyingEdgesPlaneCutter2)
-
-# set active source
-SetActiveSource(flyingEdgesPlaneCutter3)
-
-# set active source
-SetActiveSource(flyingEdgesPlaneCutter4)
-
-# set active source
-SetActiveSource(flyingEdgesPlaneCutter5)
-
-# change representation type
-flyingEdgesPlaneCutter5Display.SetRepresentationType('Surface')
-
-# set scalar coloring
-ColorBy(flyingEdgesPlaneCutter5Display, ('POINTS', 'ImageScalars'))
-
-# rescale color and/or opacity maps used to include current data range
-flyingEdgesPlaneCutter5Display.RescaleTransferFunctionToDataRange(True)
-
-# show color bar/color legend
-flyingEdgesPlaneCutter5Display.SetScalarBarVisibility(renderView1, True)
-
-# set active source
-SetActiveSource(flyingEdgesPlaneCutter6)
-
-# set active source
-SetActiveSource(flyingEdgesPlaneCutter7)
-
-# set active source
-SetActiveSource(flyingEdgesPlaneCutter8)
-
-# current camera placement for renderView1
-renderView1.CameraPosition = [-4863.596118352916, 240.2840175628662, 904.0000305175781]
-renderView1.CameraFocalPoint = [240.2840118408203, 240.2840175628662, 904.0000305175781]
-renderView1.CameraViewUp = [0.0, 0.0, 1.0]
-renderView1.CameraParallelScale = 1320.9813816144722
-
-# save screenshot
-SaveScreenshot('/Users/svh/Documents/IterateImageData/release/output.png', magnification=1, quality=100, view=renderView1)
-
-# reset view to fit data
-renderView1.ResetCamera()
-
-# reset view to fit data
-renderView1.ResetCamera()
-
-# reset view to fit data
-renderView1.ResetCamera()
-
 # reset view to fit data
 renderView1.ResetCamera()
 
@@ -449,24 +346,3 @@ renderView1.CameraParallelScale = 1303.5183166778393
 # save screenshot
 SaveScreenshot('/Users/svh/Documents/IterateImageData/release/output.png', magnification=1, quality=100, view=renderView1)
 
-# set active source
-SetActiveSource(flyingEdgesPlaneCutter9)
-
-# set active source
-SetActiveSource(test1p0vti)
-
-# destroy flyingEdgesPlaneCutter9
-Delete(flyingEdgesPlaneCutter9)
-del flyingEdgesPlaneCutter9
-
-#### saving camera placements for all active views
-
-# current camera placement for renderView1
-renderView1.CameraPosition = [3339.424013211879, 2506.9102932122582, 4316.607051459358]
-renderView1.CameraFocalPoint = [240.2840194702148, 240.284019470215, 1057.2907943725588]
-renderView1.CameraViewUp = [-0.2975132749878619, 0.8928702006568774, -0.3380364713828146]
-renderView1.CameraParallelScale = 1303.5183166778393
-
-#### uncomment the following to render all views
-# RenderAllViews()
-# alternatively, if you want to write images, you can use SaveScreenshot(...).
